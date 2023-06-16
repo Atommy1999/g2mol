730e1a90a9232a9d5566d14c7f26c5686e6b66d1
Arata TOYAMA <info@atommy.me>
2023-06-16 23:45:22 +0900

    change width of form

---
+++ components/G2Mol.jsx
@@ -0,0 +1,130 @@
+import React, { useState, useEffect } from "react";
+
+const G2Mol = () => {
+  const [mass, setMass] = useState(0);
+  const [basePairCount, setBasePairCount] = useState(1);
+  const [molar, setMolar] = useState(0);
+
+  const [massUnit, setMassUnit] = useState("ng");
+  const [bpUnit, setBpUnit] = useState("bp");
+  const [molUnit, setMolUnit] = useState("pmol");
+
+  useEffect(() => {
+    const moles = calculateMoles(mass, basePairCount);
+    setMolar(moles);
+  }, [mass, basePairCount, massUnit, bpUnit, molUnit]);
+
+  function calculateMoles(mass, basePairCount) {
+    const gPerMol = 660;
+
+    let g;
+    switch (massUnit) {
+      case "ng":
+        g = mass * Math.pow(10, -9);
+        break;
+      case "ug":
+        g = mass * Math.pow(10, -6);
+        break;
+      default:
+        g = mass;
+    }
+
+    let bp;
+    switch (bpUnit) {
+      case "bp":
+        bp = basePairCount;
+        break;
+      case "kbp":
+        bp = basePairCount * Math.pow(10, 3);
+        break;
+      case "Mbp":
+        bp = basePairCount * Math.pow(10, 6);
+        break;
+      default:
+        bp = basePairCount;
+    }
+
+    const basePairMolWeight = gPerMol * bp;
+    const moles = g / basePairMolWeight;
+
+    let molRes;
+    switch (molUnit) {
+      case "fmol":
+        molRes = moles * Math.pow(10, 15);
+        break;
+      case "pmol":
+        molRes = moles * Math.pow(10, 12);
+        break;
+      case "nmol":
+        molRes = moles * Math.pow(10, 9);
+        break;
+      default:
+        molRes = moles;
+    }
+
+    return molRes;
+  }
+
+  return (
+    <div className="flex flex-col space-y-3 max-w-lg">
+      <div className="flex flex-row justify-between items-center">
+        <span>質量 (Mass):</span>
+        <div>
+          <input
+            type="number"
+            value={mass}
+            onChange={(e) => setMass(Number(e.target.value))}
+            className="w-24 p-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-300 text-right"
+          />
+          <select
+            className="w-16"
+            value={massUnit}
+            onChange={(e) => setMassUnit(e.target.value)}
+          >
+            <option value="ng">ng</option>
+            <option value="ug">ug</option>
+          </select>
+        </div>
+      </div>
+      <div className="flex flex-row justify-between items-center">
+        <span>塩基数 (Base Pair Count):</span>
+        <div>
+          <input
+            type="number"
+            value={basePairCount}
+            onChange={(e) => setBasePairCount(Number(e.target.value))}
+            className="w-24 p-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-300 text-right"
+          />
+          <select
+            className="w-16"
+            value={bpUnit}
+            onChange={(e) => setBpUnit(e.target.value)}
+          >
+            <option value="bp">bp</option>
+            <option value="kbp">kbp</option>
+            <option value="Mbp">Mbp</option>
+          </select>
+        </div>
+      </div>
+      <div className="flex flex-row justify-between items-center">
+        <span>モル数 (Molar):</span>
+        <div>
+          <p className="text-xl font-semibold text-right inline mr-1">
+            {molar.toFixed(3)}
+          </p>
+          <select
+            className="w-16"
+            value={molUnit}
+            onChange={(e) => setMolUnit(e.target.value)}
+          >
+            <option value="fmol">fmol</option>
+            <option value="pmol">pmol</option>
+            <option value="nmol">nmol</option>
+          </select>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default G2Mol;
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,12 @@
 import Head from "next/head";
 
-import Pmol2Ng from "../components/Pmol2Ng";
+import G2Mol from "../components/G2Mol";
 
 export default function Home() {
   return (
     <div>
       <Head>
-        <title>dsDNA 換算(ng → pmol)</title>
+        <title>dsDNA 換算(g → mol)</title>
         <meta
           name="description"
           content="質量と塩基対数を入力すると、モル数が自動的に計算されます。モル数はピコモル単位で表示されます。"
@@ -16,74 +16,72 @@ export default function Home() {
 
       <main className="main">
         <div className="container">
-          <h1 className="mb-1">2本鎖DNA (dsDNA) 換算ツール (ng → pmol) </h1>
+          <h1 className="mb-1">2本鎖DNA (dsDNA) 換算ツール (g → mol) </h1>
           <p className="text-gray-500">
-            Double-stranded DNA (dsDNA) Conversion Tool (ng → pmol)
+            Double-stranded DNA (dsDNA) Conversion Tool (g → mol)
           </p>
 
-          <Pmol2Ng />
+          <G2Mol />
           <h2 className="mb-3">使い方</h2>
           <ol>
             <li>
               <strong>質量入力</strong> <br />
-              まず、DNAサンプルの質量をナノグラム単位で入力します。テキストボックスに数値を直接入力するか、上下矢印を使用して数値を調整できます。
+              まず、DNAサンプルの質量を入力します。プルダウンメニューから単位を選択することができます。
             </li>
             <li>
               <strong>塩基対数入力</strong> <br />
-              次に、DNAサンプルの1分子あたりの塩基対数を入力します。テキストボックスに数値を直接入力するか、上下矢印を使用して数値を調整できます。
+              次に、DNAサンプルの1分子あたりの塩基対数を入力します。プルダウンメニューから単位を選択することができます。
             </li>
             <li>
               <strong>モル数の計算</strong> <br />
-              本サイトでは、DNAのモル数は以下の式により計算されます：
+              本サイトでは、DNAのモル数は、例えば、以下の式により計算されます：
               <pre>
                 moles of DNA [pmol] = mass [ng] * 10^(-9) / (660 * base pair
-                count[bp]) * 10^12
+                count [bp]) * 10^12
               </pre>
               ここで660は、DNAの塩基対1つあたりの分子量 (平均値)
               を示しています。塩基対数とこの値を掛け合わせることで、DNA分子全体の分子量を求めることができます。その後、DNAの質量をこの分子量で割ることで、DNAのモル数が求まります。
             </li>
             <li>
               <strong>リアルタイム計算</strong> <br />
-              質量と塩基対数を入力すると、モル数が自動的に計算されます。モル数はピコモル単位で表示されます。
+              質量と塩基対数を入力すると、モル数が自動的に計算されます。プルダウンメニューから単位を選択することができます。
             </li>
           </ol>
           <h2 className="mb-3">How to Use</h2>
           <ol>
             <li>
               <strong>Mass Input</strong> <br />
-              First, enter the mass of your DNA sample in nanograms. You can
-              either directly enter a number into the textbox, or adjust the
-              value using the up and down arrows.
+              First, enter the mass of your DNA sample. You can select the unit
+              from the dropdown menu.
             </li>
             <li>
               <strong>Base Pair Input</strong> <br />
-              Next, enter the number of base pairs per molecule in your DNA
-              sample. You can either directly enter a number into the textbox,
-              or adjust the value using the up and down arrows.
+              Next, enter the number of base pairs per molecule of your DNA
+              sample. You can select the unit from the dropdown menu.
             </li>
             <li>
               <strong>Molar Calculation</strong> <br />
-              On this site, the number of moles of DNA is calculated using the
-              following formula:
+              On this site, the molar quantity of DNA is calculated, for
+              example, using the following formula:
               <pre>
                 moles of DNA [pmol] = mass [ng] * 10^(-9) / (660 * base pair
-                count[bp]) * 10^12
+                count [bp]) * 10^12
               </pre>
-              Here, 660 represents the average molecular weight per base pair of
-              DNA. By multiplying the number of base pairs by this value, we can
-              calculate the total molecular weight of the DNA molecule. The
-              number of moles of DNA is then calculated by dividing the mass of
-              the DNA by this molecular weight.
+              Here, 660 represents the molecular weight (average value) per base
+              pair of DNA. By multiplying the number of base pairs by this
+              value, we can obtain the molecular weight of the entire DNA
+              molecule. Then, by dividing the mass of the DNA by this molecular
+              weight, we can calculate the molar quantity of the DNA.
             </li>
             <li>
-              <strong>Real-time Calculation</strong> <br />
-              When you enter the mass and number of base pairs, the number of
-              moles is calculated automatically. The number of moles is
-              displayed in picomoles.
+              <strong>Real-Time Calculation</strong> <br />
+              As you enter the mass and the number of base pairs, the molar
+              quantity is automatically calculated. You can select the unit from
+              the dropdown menu.
             </li>
           </ol>
 
-          <h2>Author information</h2>
+          <h2 className="mb-3">Author information</h2>
 
           <ul>
             <li>
